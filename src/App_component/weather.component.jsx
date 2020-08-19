23ecb79f3abd963e85949a45dacceeea59e325b9
Ilari <ilari.matikka@gmail.com>
2020-08-20 02:31:26 +0300

    First commit

---
+++ src/App_component/weather.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Weather = () => {
+    /*container class*/
+    return (
+        <div className="container">
+            <h1>React Weather</h1>
+        </div>
+    );
+}
+
+
+export default Weather;
